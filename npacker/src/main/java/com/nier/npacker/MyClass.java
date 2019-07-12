30f20c5d81c9c713a6636546d568677c94253fcb
Nier <fgd1014074315@gmail.com>
2019-07-12 15:07:03 +0800

    修改整体目录结构

--- npacker/src/main/java/com/nier/npacker/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.nier.npacker;
-
-public class MyClass {
-    public static void main(String[] args) {
-        new TestKotlin().printHello();
-    }
-}
--- packer/src/main/kotlin/com/nier/packer/Const.kt
+++
@@ -1,9 +0,0 @@
-package com.nier.packer
-
-/**
- * Created by Nier
- * Date 2018/8/8
- */
-const val ROOT_TASK_NAME: String = "tzzb"
-const val PACK_TASK_PREFIX: String = "tzzb"
-
